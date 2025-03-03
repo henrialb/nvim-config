2de4b52766027ad0a58741832a9ca3aaa3d2567b
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-03-03 11:59:35 -0500

    Improve checking if autocommand should run
    Instead of checking for a biome config file in the root of the project,
    checks if Biome is an active LSP for the current buffer.

--- lua/h/autocommands.lua
+++ lua/h/autocommands.lua
@@ -1,26 +1,21 @@
--- Autocommand to run biome lint on save. This is needed to sort classes automatically because the
--- Biome useSortedClasses option is considered unsafe
+-- Autocommand to run biome lint on save with the `--unsafe` flag. This is needed to sort classes
+-- automatically because the Biome useSortedClasses option is considered unsafe.
 vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = {
-    "*.astro",
-    "*.css",
-    "*.graphql",
-    "*.js",
-    "*.jsx",
-    "*.json",
-    "*.jsonc",
-    "*.svelte",
-    "*.ts",
-    "*.tsx",
-    "*.vue",
-  },
   callback = function()
-    local file_path = vim.fn.expand("%:p") -- Get the full path of the current file
-    local project_root = vim.fn.fnamemodify(file_path, ":h") -- Get the directory of the current file
-    local biome_config_file = project_root .. "/biome.json" -- Adjust this if your config file is named differently
+    -- Check if the Biome LSP is active for the current buffer
+    local clients = vim.lsp.get_clients()
+    local biome_lsp_active = false
 
-    -- Check if the biome configuration file exists
-    if vim.fn.filereadable(biome_config_file) then
+    for _, client in ipairs(clients) do
+      if client.name == "biome" and client.attached_buffers[vim.fn.bufnr()] then
+        biome_lsp_active = true
+        break
+      end
+    end
+
+    if biome_lsp_active then
+      local file_path = vim.fn.expand("%:p") -- Get the full path of the current file
+
       vim.fn.system({ "bunx", "@biomejs/biome", "lint", file_path, "--unsafe" })
       local exit_code = vim.v.shell_error -- Get the exit code of the last command
 
