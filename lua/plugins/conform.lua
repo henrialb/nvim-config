bb8253ae9414f2624b3fd3454716cbf6c8d196c9
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-04-29 20:05:32 +1000

    Fix conform config
    Biome was being used even when the project used prettier. Now it checks
    for the presence of a prettier config file and uses prettier to format
    if it exists, otherise it defaults to Biome.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,15 +1,22 @@
+local function has_prettier_config()
+  local filepath = vim.fn.getcwd() .. "/.prettierrc"
+  return vim.fn.filereadable(filepath) == 1
+end
+
+local biome_or_prettier = has_prettier_config() and { "prettierd" } or { "biome" }
+
 return {
   "stevearc/conform.nvim",
   opts = {
     event = { "BufReadPre", "BufNewFile" },
     formatters_by_ft = {
-      javascript = { "biome", "prettierd", stop_after_first = true },
-      typescript = { "biome", "prettierd", stop_after_first = true },
-      typescriptreact = { "biome", "prettierd", stop_after_first = true },
-      javascriptreact = { "biome", "prettierd", stop_after_first = true },
-      json = { "biome", "prettierd", stop_after_first = true },
+      javascript = biome_or_prettier,
+      typescript = biome_or_prettier,
+      typescriptreact = biome_or_prettier,
+      javascriptreact = biome_or_prettier,
+      json = biome_or_prettier,
       html = { "prettierd" },
-      css = { "biome", "prettierd", stop_after_first = true },
+      css = biome_or_prettier,
       scss = { "prettierd" },
       markdown = { "prettierd" },
       yaml = { "yamlfix" },
@@ -17,7 +24,7 @@ return {
       bash = { "shfmt" },
       sh = { "shfmt" },
       python = { "black" },
-      graphql = { "biome", "prettierd", stop_after_first = true },
+      graphql = biome_or_prettier,
       eruby = { "erb_format" },
     },
 
